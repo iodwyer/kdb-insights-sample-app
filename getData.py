9f5d4c51c7072465f8b01d68e586daa48b99b51e
Ian O'Dwyer <iodwyer@kx.com>
2022-11-03 21:19:13 +0000

    getData pykx working

--- getData.py
+++ getData.py
@@ -2,10 +2,11 @@ import pykx as kx
 import datetime
 import pytz
 
-gw = kx.QConnection(host='localhost', port=49164)
+gw = kx.QConnection(host='localhost', port=49164, no_ctx = True)                    ## SG Gateway port
+# gw = kx.QConnection(host='localhost', port=5555, no_ctx = True)
 # gw = kx.q('hopen 49164')
 
-START_TIME = datetime.datetime.now(tz=pytz.utc) - datetime.timedelta(minutes = 15)  ## 15 Mins ago
+START_TIME = datetime.datetime.now(tz=pytz.utc) - datetime.timedelta(minutes = 36000)  ## 15 Mins ago
 END_TIME = datetime.datetime.now(tz=pytz.utc)                                       ## Now
 
 query_params = {
@@ -16,6 +17,8 @@ query_params = {
 
 empty_dict = {'':''}
 
-tab = gw('.kxi.getData', query_params, 'f', empty_dict).pd()
+tab = gw(kx.SymbolAtom('.kxi.getData'), query_params, 'f', empty_dict)
 
-print(tab)
+data = tab[1].pd()
+
+print(data)
