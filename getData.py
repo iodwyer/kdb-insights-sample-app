954ed6b4531367f50db8ce8bd81bd6d6d27b3d43
Ian O'Dwyer <iodwyer@kx.com>
2022-11-03 21:00:45 +0000

    add alternative hopen method

--- getData.py
+++ getData.py
@@ -2,7 +2,8 @@ import pykx as kx
 import datetime
 import pytz
 
-gw = kx.QConnection(host='localhost', port=49162)
+gw = kx.QConnection(host='localhost', port=49164)
+# gw = kx.q('hopen 49164')
 
 START_TIME = datetime.datetime.now(tz=pytz.utc) - datetime.timedelta(minutes = 15)  ## 15 Mins ago
 END_TIME = datetime.datetime.now(tz=pytz.utc)                                       ## Now
@@ -18,4 +19,3 @@ empty_dict = {'':''}
 tab = gw('.kxi.getData', query_params, 'f', empty_dict).pd()
 
 print(tab)
-
