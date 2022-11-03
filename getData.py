ded8eda084edd2cac8c040d6c9e4f5b62a27d530
Ian O'Dwyer <iodwyer@kx.com>
2022-11-03 20:51:13 +0000

    simplify datetime

--- getData.py
+++ getData.py
@@ -2,10 +2,10 @@ import pykx as kx
 import datetime
 import pytz
 
-gw = kx.QConnection(host='localhost', port=5001)
+gw = kx.QConnection(host='localhost', port=49162)
 
-START_TIME = (datetime.datetime.now(tz=pytz.utz) - datetime.timedelta(minutes = 15)).strftime("%Y.%m.%dD%H:%M:%S")  ## 15 Mins ago
-END_TIME = datetime.datetime.now(tz=pytz.utc).strftime("%Y.%m.%dD%H:%M:%S")  ## Now
+START_TIME = datetime.datetime.now(tz=pytz.utc) - datetime.timedelta(minutes = 15)  ## 15 Mins ago
+END_TIME = datetime.datetime.now(tz=pytz.utc)                                       ## Now
 
 query_params = {
     'table': 'trade',
@@ -18,5 +18,4 @@ empty_dict = {'':''}
 tab = gw('.kxi.getData', query_params, 'f', empty_dict).pd()
 
 print(tab)
-# gw('.kxi.getData', pytab, ['col1'], 1).pd()
 
