df03c296689dafa239bb21490aa22ce9c7ecfae3
sbruce01 <sbruce1@kx.com>
2023-03-03 14:05:15 +1100

    Adding VWAP aggregation

--- docker/cfg/python_sp.py
+++ docker/cfg/python_sp.py
@@ -17,10 +17,9 @@ import numpy as np
 #   .qsp.write.toProcess[.qsp.use `handle`target`spread!(`$raze ":",(.Q.opt[.z.x] `ip_address),":",(.Q.opt[.z.x] `tp_port);`.u.upd;1b)]
 #   / .qsp.write.toConsole[]
 
-def func(data):
+def ohlcv_agg(data):
     df = data.pd()
     # create datetime column
-    # df['time'] = df['date'] + df['time'].dt.floor('min')
     df['time'] = df['time'].dt.floor('min')
     # group by sym and datetime, and aggregate
     agg = {
@@ -28,29 +27,56 @@ def func(data):
         'size': 'sum'
     }
     ohlcv = df.groupby(['sym', 'time']).agg(agg)
-
     # rename columns to match KDB/Q query
     ohlcv.columns = ['open', 'high', 'low', 'close', 'volume']
-
     # reset index to make columns sym and datetime
     ohlcv = ohlcv.reset_index()[['sym', 'time', 'open', 'high', 'low', 'close', 'volume']]
+    ohlcv = ohlcv.astype({'open':'float', 'high':'float','low':'float','close':'float','volume':'int64'})
+    # print(ohlcv)
     tab = kx.toq.from_pandas_dataframe(ohlcv)
     return tab
 
-ohlcv_pipeline = (sp.read.from_stream(table='trade',stream="data", prefix="rt-")
-    | sp.window.tumbling(period = datetime.timedelta(seconds = 10), time_column = 'time', sort=True)
-    | sp.map(func)
+def vwap_agg(data):
+    df = data.pd()
+
+    if df.empty:
+        # print('Empty Dataframe')
+        vwap = df
+    else:
+        # create datetime column
+        df['time'] = df['time'].dt.floor('min')
+
+        # group by sym and datetime, and calculate VWAP and accumulated volume
+        agg = {
+            'price': 'mean',
+            'size': 'sum'
+        }
+        vwap = df.groupby(['sym', 'time']).apply(lambda x: pd.Series({
+            'vwap': (x['price'] * x['size']).sum() / x['size'].sum(),
+            'accVol': x['size'].sum()
+        }))
+        vwap = vwap.reset_index()[['sym', 'time', 'vwap', 'accVol']]
+        vwap = vwap.astype({'vwap':'float', 'accVol':'int64'})
+        print(vwap.columns)
+    tab = kx.toq.from_pandas_dataframe(vwap)
+    return tab
+
+# Define source as a single object, only allowed one stream subscription per worker so this allows us to envoke multiple pipelines
+source = sp.read.from_stream(table='trade',stream="data", prefix="rt-")
+
+ohlcv_pipeline = (source
+    | sp.window.tumbling(period = datetime.timedelta(seconds = 60), time_column = 'time', sort=True)
+    | sp.map(ohlcv_agg)
     # | sp.write.to_console())
     | sp.write.to_stream(table='ohlcv',stream="data", prefix="rt-"))
 
-# vwap_pipeline = (sp.read.from_stream(table='trade',stream="data", prefix="rt-")
-#     # | sp.decode.json()
-#     # | sp.map('{[data] (enlist[`timestamp]!enlist `time) xcol enlist "PS*j"$data }')
-#     # | sp.map(lambda x: ('trade', x))
-#     | sp.window.tumbling(period = datetime.timedelta(seconds = 10), time_column = 'time', sort=True)
-#     | sp.map(func)
-#     # | sp.write.to_console())
-#     | sp.write.to_console())
+vwap_pipeline = (source
+    | sp.window.tumbling(period = datetime.timedelta(seconds = 60), time_column = 'time', sort=True)
+    | sp.map(vwap_agg)
+    # | sp.write.to_console())
+    # | sp.write.to_console())
+    | sp.write.to_stream(table='vwap',stream="data", prefix="rt-"))
 
-# sp.run(ohlcv_pipeline, vwap_pipeline)
-sp.run(ohlcv_pipeline)
+
+sp.run(ohlcv_pipeline, vwap_pipeline)
+# sp.run(ohlcv_pipeline)
--- docker/kdb-tick/tick/sym.q
+++ docker/kdb-tick/tick/sym.q
@@ -8,3 +8,4 @@
 trade:([] time:"p"$(); sym:`g#`$(); price:"f"$(); size:"j"$())
 quote:([] time:"p"$(); sym:`g#`$(); bid:"f"$(); ask:"f"$(); bsize:"j"$(); asize:"j"$())
 ohlcv:([] sym:`g#`$(); time:"p"$(); open:"f"$(); high:"f"$(); low:"f"$(); close:"f"$(); volume:"j"$())
+vwap:([] sym:`g#`$(); time:"p"$(); vwap:"f"$(); accVol:"j"$())
