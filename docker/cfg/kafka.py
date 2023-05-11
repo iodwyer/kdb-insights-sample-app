88a633196c9cfd02ecf677b16be93b729c5e8c1b
Ian O'Dwyer <iodwyer@kx.com>
2023-05-11 19:41:51 +0000

    update

--- docker/cfg/kafka.py
+++ docker/cfg/kafka.py
@@ -1,8 +1,6 @@
 from kxi import sp
 
-kfk_broker  = '34.130.174.118:9091'
-
-trade_source = (sp.read.from_kafka(topic='subway', brokers=kfk_broker)
+trade_source = (sp.read.from_kafka(topic = 'subway', brokers = '34.130.174.118:9091')
     | sp.decode.json()
     | sp.write.to_console())
 
