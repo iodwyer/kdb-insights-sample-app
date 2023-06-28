c196ee9f8e63a9d9e0780c912dd55f41ca71d5a6
Ian O'Dwyer <iodwyer@kx.com>
2023-06-28 13:47:03 +0000

    comments

--- docker/cfg/taq.py
+++ docker/cfg/taq.py
@@ -18,13 +18,13 @@ quote_schema_types = {
 
 def transform_quote(data):
     dict = data.pd()
-    dict['sym'] = dict['sym'].decode()
-    dict['time'] = dict.pop('timestamp')
+    dict['sym'] = dict['sym'].decode()     ## convert to object - > symbol type
+    dict['time'] = dict.pop('timestamp')   ## rename timestamp column
     return dict
 
 quote_pipeline = (sp.read.from_kafka(topic='quote', brokers=kfk_broker)
     | sp.decode.json()
-    | sp.transform.schema(table = quote_schema_types, parse = 'auto', input_type = 'table')
+    | sp.transform.schema(table = quote_schema_types)
     | sp.map(transform_quote)
     | sp.map(lambda x: ('quote', x))
     | sp.write.to_process(handle=tp_hostport, mode='function', target='.u.upd', spread=True))
