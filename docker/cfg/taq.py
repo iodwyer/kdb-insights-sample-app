d68bdf32e393b5a210302742a952bee5cba4d697
Ian O'Dwyer <iodwyer@kx.com>
2023-06-28 13:45:19 +0000

    best we can do

--- docker/cfg/taq.py
+++ docker/cfg/taq.py
@@ -8,30 +8,25 @@ tp_hostport = ':tp:5010'
 kfk_broker  = '104.198.219.51:9091'
 
 quote_schema_types = {
-    'timestamp':       'kx.TimestampVector',
-    'sym':        'kx.SymbolVector',
-    'bid':        'kx.FloatVector',
-    'ask':        'kx.FloatVector',
-    'bsize':      'kx.IntVector',
-    'asize':      'kx.IntVector'
+    'timestamp':  kx.TimestampAtom,
+    'sym':        kx.CharAtom,
+    'bid':        kx.FloatAtom,
+    'ask':        kx.FloatAtom,
+    'bsize':      kx.IntAtom,
+    'asize':      kx.IntAtom
 }
 
 def transform_quote(data):
     dict = data.pd()
-    dict['bsize'] = int(dict['bsize'])
-    dict['asize'] = int(dict['asize'])
-    dict['time']  = dict.pop('timestamp')
-    dict['time']  = pd.to_datetime(dict['time'].decode("utf-8"))
-    dict['sym']   = dict['sym'].decode("utf-8")
+    dict['sym'] = dict['sym'].decode()
+    dict['time'] = dict.pop('timestamp')
     return dict
-
 
 quote_pipeline = (sp.read.from_kafka(topic='quote', brokers=kfk_broker)
     | sp.decode.json()
-    | sp.map('{[x] show x;x}')
-    | sp.transform.schema(quote_schema_types)
+    | sp.transform.schema(table = quote_schema_types, parse = 'auto', input_type = 'table')
+    | sp.map(transform_quote)
     | sp.map(lambda x: ('quote', x))
     | sp.write.to_process(handle=tp_hostport, mode='function', target='.u.upd', spread=True))
-
 
 sp.run(quote_pipeline)
