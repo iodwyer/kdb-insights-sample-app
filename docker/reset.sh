ecdbc141781f756e1cd82dd88e96c2948ed9110d
Ian O'Dwyer <iodwyer@kx.com>
2023-06-29 11:53:02 +0000

    tidy up 2

--- docker/reset.sh
+++ docker/reset.sh
@@ -1,7 +1,12 @@
 source rt-env.env && docker compose -f docker-compose-rt.yaml down --remove-orphans
 
 sudo rm -rf rt-data-0 publisher-1 sm-logs da-logs subscriber-1 data rt-data-session sp tplog
+ls -alrth 
+
 mkdir rt-data-0 publisher-1 sm-logs da-logs subscriber-1 data rt-data-session sp tplog
+
+ls -larth 
+
 chmod -R 777 rt-data-0 publisher-1 sm-logs da-logs subscriber-1 data rt-data-session sp tplog
 
 source rt-env.env && docker compose -f docker-compose-rt.yaml up -d 
